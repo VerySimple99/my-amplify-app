991934e1f8faa6e24120b7169df323c8b95290e9
VerySimple99 <lyricrain.seo@gmail.com>
2025-10-02 11:16:55 +0900

    feat: App.jsx h1 타이틀 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello CI/CD AWS Amplify + Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
